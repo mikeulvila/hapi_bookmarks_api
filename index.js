e5f5da29d181e0dcc07660f259ca8b031f01f43b
Mike Ulvila <mikeulvila@me.com>
2016-07-29 22:15:51 -0500

    add sample data and GET routes

--- index.js
+++ index.js
@@ -8,14 +8,49 @@ server.connection({
   port: 3000
 });
 
-// creating routes
+// sample data
+const bookmarks = [{
+    "_id": "534de420-2d86-11e6-b18e-4b692101e6d2",
+    "title": "CNN",
+    "url": "http://cnn.com/",
+    "created": new Date(),
+    "creator": "0a44ce1a-2cb9-11e6-b67b-9e71128cae77",
+    "upvoters": [
+
+    ]
+}, {
+    "_id": "86ed6030-2d86-11e6-b18e-4b692101e6d2",
+    "title": "Huffington Post",
+    "url": "http://www.huffingtonpost.com",
+    "created": new Date(),
+    "creator": "0a44ce1a-2cb9-11e6-b67b-9e71128cae77",
+    "upvoters": [
+
+    ]
+}];
+
+// routes needed
+// GET /bookmarks
 server.route({
   method: 'GET',
-  path: '/',
+  path: '/bookmarks',
   handler: (request, reply) => {
-    return reply('Hello Hapi!');
+    return reply(bookmarks);
   }
 });
+// GET /bookmarks/:id
+server.route({
+  method: 'GET',
+  path: '/bookmarks/{id}',
+  handler: (request, reply) => {
+    return reply(bookmarks[0]);
+  }
+});
+// POST /bookmarks
+// PATCH /bookmarks/:id
+// DELETE /bookmarks/:id
+// POST /bookmarks/:id/upvote
+
 
 // register good plugin and start server
 server.register({
