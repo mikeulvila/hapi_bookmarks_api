e4e6b559e03649ee4968392e806d740ab567de92
Mike Ulvila <mikeulvila@me.com>
2016-07-31 21:34:55 -0500

    define validate function

--- plugins/auth.js
+++ plugins/auth.js
@@ -2,7 +2,30 @@
 
 exports.register = function (server, options, next) {
 
+  const db = server.plugins['db'].db;
+
+  const validateFunction = (token, callback) {
+
+    db.users.findOne({token: token}, (err, user) => {
+
+      if (err) {
+        return callback(err, false);
+      }
+
+      if (!user) {
+        return callback(null, false);
+      }
+
+      return callback(null, true, user);
+    });
+  };
+
+  server.auth.strategy('bearer', 'bearer-access-token', {
+    validateFunc: validateFunction
+  })
+
    return next();
+  }
 };
 
 exports.register.attributes = {
