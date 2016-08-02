f456450bd0ad625769fcf0f559b1554dbffedaf5
Mike Ulvila <mikeulvila@me.com>
2016-08-01 22:33:59 -0500

    add auth route file

---
+++ routes/auth.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const Boom = require('boom');
+const Bcrypt = require('bcrypt');
+const Joi = require('joi');
+
+exports.register = function(server, options, next) {
+
+  const db = server.plugins['db'].db;
+
+  server.route({
+    method: 'POST',
+    path: '/login',
+    handler: function (request, reply) {
+
+      db.users.findOne({
+        username: request.payload.username
+      }, (err, user) => {
+
+        if (err) {
+          throw err;
+        }
+
+        if (!user) {
+          return reply(Boom.notAuthorized());
+        }
+
+        Bcrypt.compare(request.payload.password, user.password, (err, res) => {
+
+          if (err) {
+            throw err;
+          }
+
+          if (!res) {
+            return reply(Boom.notAuthorized());
+          }
+
+          return reply({
+            token: user.token,
+            username: user.username
+          });
+        });
+      });
+      return reply('');
+    },
+    config: {
+      validate: {
+        payload: {
+          username: Joi.string().min(1).max(50).required(),
+          password: Joi.string().min(1).max(50).required()
+        }
+      }
+    }
+
+  });
+
+  return next();
+};
+
+exports.register.attributes = {
+  name: 'routes-auth',
+  dependencies: 'db'
+}
